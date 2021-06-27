7d6996e053e198d8bbd660bc4920257a730a8d4f
Sergii Pylypenko <x.pelya.x@gmail.com>
2021-06-28 02:52:25 +0300

    Minor improvements

--- compile.sh
+++ compile.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-rm -f Giant_Cats.autogen.nml
+rm -rf generated
+mkdir -p generated
 
-cat Giant_Cats_header.nml >> Giant_Cats.autogen.nml
+cat Giant_Cats_header.nml >> generated/Giant_Cats.autogen.nml
 
-./generate-sprite-layout.py cat_1 >> Giant_Cats.autogen.nml
+./generate-sprite-layout.py cat_1 12 14 -128 128 >> generated/Giant_Cats.autogen.nml
 
-cat Giant_Cats_footer.nml >> Giant_Cats.autogen.nml
+cat Giant_Cats_footer.nml >> generated/Giant_Cats.autogen.nml
 
-nmlc -c --default-lang=english.lng --grf=Giant_Cats.grf Giant_Cats.autogen.nml || exit 1
+nmlc -c --default-lang=english.lng --grf=Giant_Cats.grf generated/Giant_Cats.autogen.nml || exit 1
 
 cp -f *.grf $HOME/.local/share/openttd/newgrf/
 
--- generate-sprite-layout.py
+++ generate-sprite-layout.py
@@ -3,19 +3,22 @@
 import os, sys
 
 name = sys.argv[1]
+w = int(sys.argv[2])
+h = int(sys.argv[3])
+xoff = int(sys.argv[4])
+yoff = int(sys.argv[5])
 
-for x in range(15):
-	for y in range(15):
+for x in range(w):
+	for y in range(h):
 		print('')
 		print('spriteset(spriteset_%s_%d_%d, "gfx/empty.png") { template_empty() }' % (name, x, y))
 		print('alternative_sprites(spriteset_%s_%d_%d, ZOOM_LEVEL_IN_4X, BIT_DEPTH_32BPP, "gfx/%s.png") {[' % (name, x, y, name))
-		#print('	%d, %d, 128, 128, -64, -64 ]}' % (x * 128 + y * 128, 1920 + x * 64 - y * 64))
-		print('	%d, %d, 128, 128, 0, 0 ]}' % (1920 + x * 128 - y * 128, x * 64 + y * 64))
-		print('spritelayout spritelayout_%s_%d_%d { ground { sprite: GROUNDSPRITE_NORMAL; } building { sprite: spriteset_%s_%d_%d; } }' % (name, x, y, name, x, y))
+		print('	%d, %d, 128, 128, 0, 0 ]}' % (1920 + x * 128 - y * 128 + xoff, x * 64 + y * 64 + yoff))
+		print('spritelayout spritelayout_%s_%d_%d { ground { sprite: LOAD_TEMP(0) + LOAD_TEMP(1); } building { sprite: spriteset_%s_%d_%d; } }' % (name, x, y, name, x, y))
 
 print('')
 print('switch(FEAT_OBJECTS, SELF, switch_spritelayout_%s, relative_pos) {' % (name))
-for x in range(15):
-	for y in range(15):
+for x in range(w):
+	for y in range(h):
 		print('0x%02X%02X: spritelayout_%s_%d_%d;' % (x, y, name, x, y))
 print('}')
