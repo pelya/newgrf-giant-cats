45a0824b108c0e998552a025f0c363d92b88bafd
Sergii Pylypenko <x.pelya.x@gmail.com>
2021-07-03 02:04:17 +0300

    Cat 5 animated

--- compile.sh
+++ compile.sh
@@ -13,6 +13,12 @@ cat Giant_Cats-header.nml >> generated/Giant_Cats.autogen.nml
 
 ./generate-sprite-layout.py cat_4 15 15 -96 128 trees >> generated/Giant_Cats.autogen.nml
 
+./generate-sprite-layout.py cat_5_1 12 12 128 256 no-trees >> generated/Giant_Cats.autogen.nml
+./generate-sprite-layout.py cat_5_2 12 12 128 256 no-trees >> generated/Giant_Cats.autogen.nml
+./generate-sprite-layout.py cat_5_3 12 12 128 256 no-trees >> generated/Giant_Cats.autogen.nml
+./generate-sprite-layout.py cat_5_4 12 12 128 256 no-trees >> generated/Giant_Cats.autogen.nml
+./generate-sprite-layout.py cat_5_5 12 12 128 256 no-trees >> generated/Giant_Cats.autogen.nml
+
 cat Giant_Cats.nml >> generated/Giant_Cats.autogen.nml
 
 nmlc -c --default-lang=english.lng --grf=Giant_Cats.grf generated/Giant_Cats.autogen.nml || exit 1
